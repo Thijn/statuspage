2c386379c415b348ddc92ecd87d315465d423605
Monitoring <monitoring@thijn.ovh>
2021-12-06 15:39:08 +0100

    [Automated] Update Health Check Logs

--- health-check.sh
+++ health-check.sh
@@ -17,8 +17,8 @@ while read -r line
 do
   echo "  $line"
   IFS='=' read -ra TOKENS <<< "$line"
-  KEYSARRAY+=(${TOKENS[0]})
-  URLSARRAY+=(${TOKENS[1]})
+  KEYSARRAY+=("${TOKENS[0]}")
+  URLSARRAY+=("${TOKENS[1]}")
 done < "$urlsConfig"
 
 echo "***********************"
@@ -32,10 +32,10 @@ do
   url="${URLSARRAY[index]}"
   echo "  $key=$url"
 
-  for i in 1 2 3 4; 
+  for i in 1 2 3 4;
   do
     response=$(curl --write-out '%{http_code}' --silent --output /dev/null $url)
-    if [ "$response" -eq 200 ] || [ "$response" -eq 202 ] || [ "$response" -eq 301 ] || [ "$response" -eq 307 ]; then
+    if [ "$response" -eq 200 ] || [ "$response" -eq 202 ] || [ "$response" -eq 301 ] || [ "$response" -eq 302 ]; then
       result="success"
     else
       result="failed"
@@ -57,8 +57,8 @@ done
 if [[ $commit == true ]]
 then
   # Let's make Vijaye the most productive person on GitHub.
-  git config --global user.name 'Vijaye Raji'
-  git config --global user.email 'vijaye@statsig.com'
+  git config --global user.name 'Monitoring'
+  git config --global user.email 'monitoring@thijn.ovh'
   git add -A --force logs/
   git commit -am '[Automated] Update Health Check Logs'
   git push
